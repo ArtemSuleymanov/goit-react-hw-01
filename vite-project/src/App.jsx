06a4c5db0666e5d55810882a041789dd8df25483
Artem <suleymanov2007artem@gmail.com>
2025-01-11 19:45:15 +0200

    doing all homework task

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,23 @@
+import FriendList from "./components/friendList/FriendList";
+import Profile from "./components/profile/Profile";
+import userData from "./userData.json";
+import friends from "./friends.json";
+import transactions from "./transactions.json";
+import TransactionHistory from "./components/transactionHistory/TransactionHistory";
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
 };
 export default App;
---
+++ vite-project/src/components/transactionHistory/TransactionHistory.jsx
@@ -0,0 +1,28 @@
+import TransactionHistoryItem from "./TransactionHistoryItem";
+import styles from "./TransactionHistory.module.css";
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={styles.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((transaction) => (
+          <TransactionHistoryItem
+            key={transaction.id}
+            type={transaction.type}
+            amount={transaction.amount}
+            currency={transaction.currency}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
